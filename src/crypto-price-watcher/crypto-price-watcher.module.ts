5357f3ab0ca342230aaa0ce4344b79598ddfb5d1
enyelberth <enyelberthrc22.z@gmail.com>
2026-02-21 11:16:40 -0400

    feat:refactor crypto way

--- src/crypto-price-watcher/crypto-price-watcher.module.ts
+++ src/crypto-price-watcher/crypto-price-watcher.module.ts
@@ -3,9 +3,11 @@ import { PrismaClient } from '@prisma/client';
 import { AccountService } from 'src/account/account.service';
 import { BinanceService } from 'src/binance/binance.service';
 import { TradingService } from 'src/trading/trading.service';
+import { CryptoPriceWatcherGateway } from './crypto-price-watcher.gateway';
+import { IndicatorsService } from 'src/indicators/indicators.service';
 
 @Module({
   exports: [TradingService],
-  providers: [TradingService, BinanceService,AccountService, PrismaClient],
+  providers: [TradingService, IndicatorsService,CryptoPriceWatcherGateway,BinanceService,AccountService, PrismaClient],
 })
 export class CryptoPriceWatcherModule {}
